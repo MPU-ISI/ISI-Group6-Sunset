0d2d2ea82946829c8121450fc51dcfc741b304ec
JamelNOB <1872299302@qq.com>
2025-02-20 22:19:59 +0800

    Multiple Photos per Product

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -13,6 +13,7 @@ const reviewSchema = new mongoose.Schema(
 const prodctSchema = new mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
+  additionalImages: [{ type: String }],
   brand: { type: String, required: true },
   price: { type: Number, default: 0, required: true },
   category: { type: String, required: true },
