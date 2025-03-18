010681f32be7e6d424f0dcc9ade69d1ebcaa7723
REIkd <p2211681@mpu.edu.mo>
2025-03-18 19:06:09 +0800

    Order

--- Full_Stack_Ecommerce/backend/models/OrderStatus.js
+++
@@ -1,9 +0,0 @@
-// 文件位置：/server/models/OrderStatus.js
-const mongoose = require('mongoose');
-
-const OrderStatusSchema = new mongoose.Schema({
-  statusID: { type: Number, required: true, unique: true },
-  statusName: { type: String }
-});
-
-module.exports = mongoose.model('OrderStatus', OrderStatusSchema);
